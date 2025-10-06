cade0c1e91beef3e02c51c297617adbb0bd49df3
edgbe <edegbe@eugeniaschool.com>
2025-10-06 13:05:19 +0000

    main_4 first version

---
+++ main_4.py
@@ -0,0 +1,32 @@
+# TODO: Create a string variable with a sentence
+
+presentation = " je suis elom degbe"
+
+# TODO: Convert the string to uppercase
+
+print(presentation.upper())
+
+# TODO: Convert the string to lowercase
+
+print(presentation.lower())
+
+# TODO: Capitalize the first letter of each word
+
+print(presentation.title())
+print(presentation.replace("je","il"))
+
+# TODO: Replace a word in the string
+
+presentation ="il"
+
+# TODO: Split the string into a list of words
+
+# TODO: Join the list of words back into a qstring using a different separator
+
+# TODO: Find the position of a specific word in the string
+
+# TODO: Check if the string starts with a specific word
+
+# TODO: Remove leading and trailing whitespace from a string
+
+# Print the results of each operation
